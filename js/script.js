39ea6d32a1537d435d857b3b1a8c2099ca0cd14a
MatteoArt <mattrim8@gmail.com>
2023-07-28 19:34:18 +0200

    completo es

--- js/script.js
+++ js/script.js
@@ -3,28 +3,24 @@
 Vue.createApp({
     data() {
         return {
-            mail: '',
             emailList: [],
         }
     },
     methods: {
         getData() {
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail"
-            ) //funzione che verrà eseguita quando axios riceve una risposta
-            .then((responseDate) => {
-                const dateObj = responseDate.data; //l'oggetto contenente i dati
-                const mail = dateObj.response; //il dato che mi interessa
-                this.mail = mail;
-            })
+            
         }
     },
     mounted() {
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail"
+        for (let i = 1; i <= 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail"
             ) //funzione che verrà eseguita quando axios riceve una risposta
-            .then((responseDate) => {
-                const dateObj = responseDate.data; //l'oggetto contenente i dati
-                const mail = dateObj.response; //il dato che mi interessa
-                this.mail = mail;
-            })
+                .then((responseDate) => {
+                    const dateObj = responseDate.data; //l'oggetto contenente i dati
+                    const mail = dateObj.response; //il dato che mi interessa
+                    this.emailList.push(mail);
+                })
+        }
+
     }
 }).mount("#app");
