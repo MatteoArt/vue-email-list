aa7b887f4d2847dccf86210bc6f7a3cb45547d5d
MatteoArt <mattrim8@gmail.com>
2023-07-28 18:25:57 +0200

    genero una mail

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 Vue.createApp({
     data() {
         return {
+            mail: '',
             emailList: [],
         }
     },
@@ -10,9 +11,20 @@ Vue.createApp({
         getData() {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail"
             ) //funzione che verrà eseguita quando axios riceve una risposta
-            .then(function(response) {
-                console.log(response);
+            .then((responseDate) => {
+                const dateObj = responseDate.data; //l'oggetto contenente i dati
+                const mail = dateObj.response; //il dato che mi interessa
+                this.mail = mail;
             })
         }
+    },
+    mounted() {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail"
+            ) //funzione che verrà eseguita quando axios riceve una risposta
+            .then((responseDate) => {
+                const dateObj = responseDate.data; //l'oggetto contenente i dati
+                const mail = dateObj.response; //il dato che mi interessa
+                this.mail = mail;
+            })
     }
 }).mount("#app");
