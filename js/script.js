6f0a075d459e39452dde219237f8e0fa5262cbcf
MatteoArt <mattrim8@gmail.com>
2023-08-01 19:09:40 +0200

    imposto es base(avevo fatto prima bonus)

--- js/script.js
+++ js/script.js
@@ -3,24 +3,10 @@
 Vue.createApp({
     data() {
         return {
-            emailList: [],
-        }
-    },
-    methods: {
-        getData() {
             
         }
     },
-    mounted() {
-        for (let i = 1; i <= 10; i++) {
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail"
-            ) //funzione che verrà eseguita quando axios riceve una risposta
-                .then((responseDate) => {
-                    const dateObj = responseDate.data; //l'oggetto contenente i dati
-                    const mail = dateObj.response; //il dato che mi interessa
-                    this.emailList.push(mail);
-                })
-        }
+    methods: {
 
     }
 }).mount("#app");
